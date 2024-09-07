4fe2d86730298d8254feb8ece44c417237866d32
ruthvij17 <130194046+ruthvij17@users.noreply.github.com>
2024-09-07 05:15:45 +0000

    Learning React

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,22 +1,34 @@
-import ClassComponent from "./components/ClassComponent";
-import FunctionComponents from "./components/FunctionComponents";
+import ClassComponent from "./pages/ClassComponent";
+import FunctionComponents from "./pages/FunctionComponents";
+import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import BaseHOC from "./HOC/BaseHOC";
 const App = () => {
   const author = "Ruthvij";
+
   const [name, setName] = useState("Chandan");
   return (
-    <div className="App">
-      <p>Class Components:</p>
-      <ClassComponent />
-      <p>Function Components</p>
-      {/* <FunctionComponents name="devtown" age={20} author={author} /> */}
-      <FunctionComponents
-        name={name}
-        age={20}
-        author={author}
-        setName={setName}
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <BaseHOC>
+            <ClassComponent />
+          </BaseHOC>
+        }
       />
-    </div>
+      <Route
+        path="/functional-components"
+        element={
+          <FunctionComponents
+            name={name}
+            age={10}
+            author={author}
+            setName={setName}
+          />
+        }
+      />
+    </Routes>
   );
 };
 
